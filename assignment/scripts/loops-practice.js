809d7e8155cf579a0af80507934019308027eeb5
Nate Notermann <nate.notermann@gmail.com>
2022-05-19 16:41:42 -0500

    finished even # strech goal 1.c

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -17,19 +17,28 @@ for (let i=0; i<6; i++) {
   console.log(i);
 }
 //------------------------------1.a DONE -------------------------------
+console.log( '--------1.a DONE ---------' );
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this? 
 console.log('count from 3 to 5');
 
 for (let i=3; i<6; i++ ) { // changed i=0 to i=3.
-  console.log(i);
+  console.log(i); 
 }
 //------------------------------1.b DONE -------------------------------
+console.log( '--------1.b DONE ---------' );
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+
+for ( let i = 1; i<11; i++ ) {   //start at 1, as long as i is less than 11(10), i increment 1
+  if ( i % 2 == 0 ) {  //if i's remainder of 2 is equal to 0, than -->
+  console.log( i );}  //console.log i
+}
+//------------------------------1.c STRECH DONE -------------------------------
+console.log( '--------1.c STRECH DONE---------' );
 
 //-----------COME BACK TO 1.C STRRETCH GOAL!!!!----!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!+!!++!-------
 
@@ -40,6 +49,8 @@ for (let i=5; i>-1; i-- ){
   console.log(i);
 }
 //------------------------------1.d DONE -------------------------------
+console.log( '--------1.d DONE ---------' );
+
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
 let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
