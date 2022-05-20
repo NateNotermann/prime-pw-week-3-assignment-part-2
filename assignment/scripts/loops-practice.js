0a8c8c1594ac20bd5c6f208489f18c44eeea5522
Nate Notermann <nate.notermann@gmail.com>
2022-05-19 19:52:34 -0500

    finished 3.a, took a WHILE. Get it? a WHILE.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -58,21 +58,61 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array //FOR OF 2
 console.log('Some stars:');
 
-for ( let star of stars ) {
-  console.log(star);
-}
-console.log('Some stars:');
-
-for ( let star of stars ) {
-  console.log(stars);
+for ( let star of stars ) { //"OF" cycles threw all of the items in the array until it gets to the end.
+  console.log('for OF stars:', star); // learned "OF" give you the actual index item VALUE
 }
 
+for(i in stars) {  //  --- just added an "FOR IN" loops for practice 
+  //not answer to question, becuase it asked or a "FOR OF" loop
+  // LEARNED "IN" just give you the INDEX of the item -----------
+  console.log( 'for IN stars:', stars[ i ] );  //you need to add the BRACKET to the current index or STARS to console.log each item
+} //just practice
 
+//------------------------------2.a DONE -------------------------------
+console.log( '//--------2.a DONE ---------' );
 // 3. While loops  
 console.log('---- 3. While loop ----');
-
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+
+//attempt 1 -----  // it does console log each star, only because it console.logs the entire array
+// while (stars) {
+//   console.log( stars );
+//   stars++;
+// }
+
+// i = 0;
+// while ( stars[i]; ) {
+//   console.log( stars[i] );
+//   i++;
+// }
+
+//---------3.a ANWERS 1 - START--------------
+console.log(' ---3.a ANWERS 1 - START---- ');
+newLength = stars.length; // make new variable, assigns it the value of the number of items in the stars array
+{ // "LET i" only works if i seperate this entire block of code with another set of curly brackets
+let i = 0; // cannont create a varaible that already exsists (in theis docuemnt) 
+//maybe i already created an i cariable in previously in thei document. YEP - THAT WAS WHY IT DIDN't WORK
+
+// i = 0; // only worked when i just wroke i = 0. 
+console.log( '"newLength" variable is:', newLength ); //console.log that value. Stars arrays has 5 items, newLength should equal stars
+while ( i < newLength ) {
+  console.log( 'listing stars items v1:', stars[i] );
+  i++;
+}
+} // "LET i" only works if i seperate this entire block of code with another set of curly brackets
+//---------3.a ANWERS 1 - STOP--------------
+
+//---------3.a ANWERS 2 - START--------------
+console.log(' ---3.a ANWERS 2 - START---- ');
+i = 0; // just ASSIGNING new VALUE to i, not creating a new variable
+console.log( '"newLength" variable is:', newLength ); //console.log that value. Stars arrays has 5 items, newLength should equal stars
+while ( i < newLength ) {
+  console.log( 'listing stars items v2:', stars[i] );
+  i++;
+} 
+//---------3.a ANWERS 2 - STOP--------------
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
